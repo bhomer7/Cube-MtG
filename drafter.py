c5ed6ccda5f37f186c83ba3809ffaae0a184aaab
Devon Richards <devon@apptimize.com>
2017-03-23 15:46:47 -0500

    Added more special casing to generation

--- drafter.py
+++ drafter.py
@@ -24,8 +24,8 @@ def main(rarity_file='rarities.yaml', dest_dir='results'):
         rarity_config = yaml.load(rarities_file)
     if rarity_config is None:
         return
-    players_with_packs = [[[] for _ in range(rarity_config['packs-per-player'])]
-                          for _ in range(rarity_config['players'])]
+    total_packs = rarity_config['packs-per-player'] * rarity_config['players']
+    packs = [[] for _ in range(total_packs)]
 
     for rarity in rarity_config['order']:
         rare_files = glob.glob('{}s/*.dec'.format(rarity))
@@ -44,28 +44,51 @@ def main(rarity_file='rarities.yaml', dest_dir='results'):
                 rare_lists[rare_index(rare_name)] = lines * duplication
         rare_indexes = [rare_index(r) for r in rare_files]
         use_rarities = [r for r in rare_indexes if r not in rarity_config[rarity].get('rem-files', [])]
-
-        rem_list = []
-        for rare_list in rarity_config[rarity].get('rem-files', []):
-            rem_list += rare_lists[rare_list]
+        for r in use_rarities:
+            random.shuffle(rare_lists[r])
 
         per_pack = rarity_config[rarity]['per-file']
+        for i, pack in enumerate(packs):
+            per_pack_with_index = {r: per_pack for r in use_rarities}
+            if isinstance(per_pack, list):
+                for selections in per_pack:
+                    usable = [r for r in selections[3] if len(rare_lists[r]) >= selections[1]]
+                    assert len(usable) >= selections[0]
+                    random.shuffle(usable)
+                    full = usable[:selections[0]]
+                    rest = [r for r in selections[3] if r not in full]
+                    for r in full:
+                        per_pack_with_index[r] = selections[1]
+                    for r in rest:
+                        per_pack_with_index[r] = selections[2]
+            for rare_list in use_rarities:
+                the_list = rare_lists[rare_list]
+                for _ in range(per_pack_with_index[rare_list]):
+                    pack.append(the_list.pop())
 
-        for rare_list in use_rarities:
-            the_list = rare_lists[rare_list]
-            random.shuffle(the_list)
-            for player_packs in players_with_packs:
-                for player_pack in player_packs:
-                    for _ in range(per_pack):
-                        player_pack.append(the_list.pop())
-            rem_list += the_list
-
-        remaining_count = rarity_config[rarity]['total'] - len(use_rarities) * per_pack
+        rem_list = []
+        for _, lst in rare_lists.items():
+            rem_list += lst
+        real_per_pack = 0
+        if isinstance(per_pack, list):
+            for selections in per_pack:
+                real_per_pack += selections[0] * selections[1]
+                real_per_pack += (len(selections[3]) - selections[0]) * selections[2]
+        else:
+            real_per_pack = per_pack * len(use_rarities)
+        remaining_count = rarity_config[rarity]['total'] - real_per_pack
         random.shuffle(rem_list)
-        for player_packs in players_with_packs:
-            for player_pack in player_packs:
-                for _ in range(remaining_count):
-                    player_pack.append(rem_list.pop())
+        for pack in packs:
+            for _ in range(remaining_count):
+                pack.append(rem_list.pop())
+
+    random.shuffle(packs)
+    players_with_packs = []
+    for i in range(rarity_config['players']):
+        player_packs = []
+        for j in range(rarity_config['packs-per-player']):
+            player_packs.append(packs.pop())
+        players_with_packs.append(player_packs)
 
     for player_id, player_packs in enumerate(players_with_packs):
         player_pool = []

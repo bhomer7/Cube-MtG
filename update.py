28f18358852bfbfcf1ca2dac4add82a569225137
Devon Richards <devon@apptimize.com>
2017-04-04 23:55:06 -0500

    Fix update.py

--- update.py
+++ update.py
@@ -3,26 +3,20 @@
 Update the full files for each rarity.
 """
 import glob
-import yaml
 
 
 def main(rarity_file='rarities.yaml'):
     """
     Create full files for each rarity.
     """
-    rarity_config = None
-    with open(rarity_file) as rarities_file:
-        rarity_config = yaml.load(rarities_file)
-    if rarity_config is None:
-        return
-
-    for rarity in rarity_config['order']:
+    for rarity in glob.glob('*s/'):
         lines = []
-        for rare_file in glob.glob('{}s/*.dec'.format(rarity)):
+        for rare_file in glob.glob('{}*.dec'.format(rarity)):
             with open(rare_file) as rfile:
                 lines += list(rfile.readlines())
-        with open('{}s.dec'.format(rarity), 'w') as out_file:
-            out_file.write(''.join(lines))
+        if len(lines) > 0:
+            with open('{}.dec'.format(rarity[:-1]), 'w') as out_file:
+                out_file.write(''.join(lines))
 
 
 if __name__ == '__main__':
